2cd929642d0e48eb8488bf2cba9a23e99b6e8d3a
hamimhossainm <hamim.dashboard@gmail.com>
2024-05-17 16:54:04 +0600

    FAQ Updated & Styled

--- JS/code.js
+++ JS/code.js
@@ -3,3 +3,11 @@ window.addEventListener("scroll",function(){
     let nav = document.querySelector("nav");
     nav.classList.toggle("window-scroll",window.scrollY>50);
 });
+
+let faqs= document.querySelectorAll(".faq");
+
+faqs.forEach(faq=>{
+    faq.addEventListener("click",()=>{
+        faq.classList.toggle("open");
+    });
+})
